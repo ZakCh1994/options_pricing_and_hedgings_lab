d0044e43d910c18a70b1948596a358c151715400
Zak <c.chourmouziadis@student.rug.nl>
2026-06-30 14:28:35 +0200

    Added short option payoff tests

--- src/options_lab/payoffs.py
+++ src/options_lab/payoffs.py
@@ -8,6 +8,12 @@ def put_payoff(spot_at_maturity, strike):
     spot_at_maturity = np.asarray(spot_at_maturity, dtype=float)
     return np.maximum(strike - spot_at_maturity, 0)
 
+def short_call_payoff(spot_at_maturity, strike):
+    return -call_payoff(spot_at_maturity, strike)
+
+def short_put_payoff(spot_at_maturity, strike):
+    return -put_payoff(spot_at_maturity, strike)
+
 def long_forward_payoff(spot_at_maturity, delivery_price):
     spot_at_maturity = np.asarray(spot_at_maturity, dtype=float)
     return spot_at_maturity - delivery_price
@@ -15,3 +21,4 @@ def long_forward_payoff(spot_at_maturity, delivery_price):
 def short_forward_payoff(spot_at_maturity, delivery_price):
     spot_at_maturity = np.asarray(spot_at_maturity, dtype=float)
     return delivery_price - spot_at_maturity
+
--- tests/test_payoffs.py
+++ tests/test_payoffs.py
@@ -4,7 +4,9 @@ from options_lab.payoffs import(
     call_payoff,
     put_payoff,
     long_forward_payoff,
-    short_forward_payoff
+    short_forward_payoff,
+    short_call_payoff,
+    short_put_payoff,
 )
 
 def test_call_payoff_for_array_input():
@@ -54,7 +56,22 @@ def test_long_and_short_forward_payoffs_are_opposites():
     long_payoff = long_forward_payoff(spots, delivery_price) 
     short_payoff = short_forward_payoff(spots, delivery_price)
 
-    expected = np.array([0, 0, 0])
-
     np.testing.assert_array_equal(long_payoff, -short_payoff)
 
+def test_long_and_short_call_options_payoffs_are_opposites():
+    spots = np.array([80, 100, 120])
+    delivery_price = 100
+
+    long_call = call_payoff(spots, delivery_price) 
+    short_call = short_call_payoff(spots, delivery_price)
+
+    np.testing.assert_array_equal(long_call, -short_call)
+
+def test_long_and_short_put_options_payoffs_are_opposites():
+    spots = np.array([80, 100, 120])
+    delivery_price = 100
+
+    long_put = put_payoff(spots, delivery_price) 
+    short_put = short_put_payoff(spots, delivery_price)
+
+    np.testing.assert_array_equal(long_put, -short_put)
